feee9877739dc707ad966ea1ca1b76f14366dd76
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-04-12 18:28:01 +0200

    No Pathname

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  const location = useLocation();
-  
   return (
     <>
       <div className="neon-stripes">
@@ -18,21 +15,11 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="nav-brand">ReverseMarket</div>
         <div className="nav-links">
-          <a href="/" className={location.pathname === '/' ? 'active' : ''}>
-            Dashboard
-          </a>
-          <a href="/azioni" className={location.pathname === '/azioni' ? 'active' : ''}>
-            Azioni
-          </a>
-          <a href="/forex" className={location.pathname === '/forex' ? 'active' : ''}>
-            Forex
-          </a>
-          <a href="/crypto" className={location.pathname === '/crypto' ? 'active' : ''}>
-            Crypto
-          </a>
-          <a href="/analisi" className={location.pathname === '/analisi' ? 'active' : ''}>
-            Analisi
-          </a>
+          <a href="/">Dashboard</a>
+          <a href="/azioni">Azioni</a>
+          <a href="/forex">Forex</a>
+          <a href="/crypto">Crypto</a>
+          <a href="/analisi">Analisi</a>
         </div>
       </nav>
     </>
